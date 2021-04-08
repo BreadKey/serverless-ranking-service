16f2c4024dff7faee6820e5d2bf92e32ef2c6d72
2breadkey <2breadkey@gmail.com>
2021-04-08 19:23:09 +0900

    refactor select query

--- repository/game_record_repository.py
+++ repository/game_record_repository.py
@@ -1,16 +1,23 @@
 import os
-from typing import List
+from typing import List, Tuple
 
 import pymysql
 from model.game_record import GameRecord
 
+from enum import Enum
+
 __RANKING_DB = pymysql.connect(
-    host=os.environ["host"],
-    user=os.environ["user"],
-    port=int(os.environ["port"]),
+    host=os.environ.get("host", "localhost"),
+    user=os.environ.get("user", "root"),
+    port=int(os.environ.get("port", 3306)),
     passwd=os.environ.get("password", ""),
-    db=os.environ["database"]
+    db=os.environ.get("database", "ranking")
 )
+
+
+class Order(Enum):
+    ASC = 1
+    DESC = 2
 
 
 def save(gameRecord: GameRecord) -> GameRecord:
@@ -26,7 +33,11 @@ def save(gameRecord: GameRecord) -> GameRecord:
 def findByGameOrderByScoreDesc(game: str, count: int) -> List[GameRecord]:
     with __RANKING_DB.cursor(pymysql.cursors.DictCursor) as cursor:
         cursor.execute(
-            f'select * from GameRecords where game = "{game}" order by score desc limit {count}')
+            __selectBuilder(GameRecord,
+                           where={"game": game},
+                           orderBy=("score", Order.DESC),
+                           limit=count)
+        )
 
     return list(map(__gameRecordFromRow, cursor.fetchall()))
 
@@ -34,7 +45,10 @@ def findByGameOrderByScoreDesc(game: str, count: int) -> List[GameRecord]:
 def findTopByGameAndUserId(game: str, userId: str) -> GameRecord:
     with __RANKING_DB.cursor(pymysql.cursors.DictCursor) as cursor:
         cursor.execute(
-            f'select * from GameRecords where game = "{game}" and userId = "{userId}" order by score desc limit 1'
+            __selectBuilder(GameRecord,
+                           where={"game": game, "userId": userId},
+                           orderBy=("score", Order.DESC),
+                           limit=1)
         )
 
     row = cursor.fetchone()
@@ -44,3 +58,33 @@ def findTopByGameAndUserId(game: str, userId: str) -> GameRecord:
 
 def __gameRecordFromRow(row) -> GameRecord:
     return GameRecord(row["game"], row["userId"], row["score"], row["id"])
+
+def __selectBuilder(
+        entity,
+        select: List[str] = [],
+        where: dict = {},
+        orderBy: Tuple[str, Order] = None,
+        limit: int = None) -> str:
+    query = "select "
+    query += '*' if len(select) == 0 else ",".join(select)
+    query += f' from {entity.__name__}s '
+
+    if (len(where) > 0):
+        query += "where "
+        query += " and ".join(
+            list(
+                map(lambda key:
+                    f'{key} = ' +
+                    (f'"{where[key]}"' if isinstance(
+                        where[key], str) else f'{where[key]}'),
+                    where)
+            )
+        )
+
+    if (orderBy is not None):
+        query += f" order by {orderBy[0]} {orderBy[1].name}"
+
+    if (limit is not None):
+        query += f" limit {limit}"
+
+    return query
